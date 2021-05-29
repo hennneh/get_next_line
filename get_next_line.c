24f2705c87841e77d968ce01509cbcb202f8eafc
hennneh <hendrik.lehmann96@gmail.com>
2021-05-29 12:48:42 +0200

    everything

--- get_next_line.c
+++ get_next_line.c
@@ -12,12 +12,36 @@
 
 #include "get_next_line.h"
 
+static int	check_buff(char **buff, char **line)
+{
+	char	*end;
+	char	*tmp;
+
+	if (!buff[fd])
+		buff[fd] = ft_strdup("");
+	if (!buff[fd])
+		return (-1);
+	end = ft_strchr(buff[fd], '\n');
+	if (end)
+	{
+		*end = 0;
+		tmp = *buff;
+		*line = ft_strdup(*buff);
+		*buff = ft_strdup(end + 1);
+		if (!(*buff)
+			return (-1);
+		free(tmp);
+		return (1);
+	}
+	return (0);
+}
+
 int	get_next_line(int fd, char **line)
 {
-	static char	save[MAX_FILES][BUFFER_SIZE + 1];
-	char		buff[BUFFER_SIZE + 1]
-	while (BUFFER_SIZE > 0 && fd >= 0 && line)
-	{
-		
-	}
+	static char	buff[MAX_FILES][BUFFER_SIZE];
+	int			i;
+
+	if (BUFFER_SIZE < 1 || fd < 0 || fd > MAX_FILES || !line)
+		return (-1);
+	i = check_buff(&buff[fd], line);
 }

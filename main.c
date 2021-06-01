6ea5e70850524aef71957f0029b990867e2c6bc1
hennneh <hendrik.lehmann96@gmail.com>
2021-06-01 16:30:01 +0200

    fixing norm issues just for peace of mind

--- main.c
+++ main.c
@@ -8,12 +8,15 @@
 
 int	main(void)
 {
-        char *line;
-        printf("buffer size = %d\n", BUFFER_SIZE);
-        int     fd = open("file3", O_RDONLY);
-        printf("fd = %d\n", fd);
-        int a = get_next_line(fd, &line);
-		printf("return-value of gnl = %d\n", a);
-		printf("\n%s\n", line);
-        return (0);
+	char	*line;
+	int		fd;
+	int		a;
+
+	printf("buffer size = %d\n", BUFFER_SIZE);
+	fd = open("file3", O_RDONLY);
+	printf("fd = %d\n", fd);
+	a = get_next_line(fd, &line);
+	printf("return-value of gnl = %d\n", a);
+	printf("\n%s\n", line);
+	return (0);
 }

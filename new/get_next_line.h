c40b94c9551c8150915f4ed7199ddfce534b1744
hennneh <hendrik.lehmann96@gmail.com>
2021-06-03 20:15:30 +0200

    everything

--- new/get_next_line.h
+++ new/get_next_line.h
@@ -4,11 +4,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
+
 int		get_next_line(int fd, char **line);
 char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 size_t	ft_strlen(const char *str);
-void	ft_bzero(void *s, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 

86df84faa5e825a4b04c73714df982741f89ffd0
hennneh <hendrik.lehmann96@gmail.com>
2021-06-04 14:14:10 +0200

    adding headers

--- new/get_next_line.c
+++ new/get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlehmann <hlehmann@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 16:54:48 by hlehmann          #+#    #+#             */
+/*   Updated: 2021/02/13 16:57:26 by hlehmann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 static char	*ft_line(char *buff, char **line, int readcount)
@@ -44,14 +56,12 @@ static char	*ft_join(char *temp, char *buff)
 	return (buff);
 }
 
-int	get_next_line(int fd, char **line)
+int	ft_check(int fd, char **line, char **buff, char *temp)
 {
-	static char	*buff[MAX_FD];
-	char		temp[BUFFER_SIZE + 1];
 	int			readcount;
 
 	readcount = read(fd, temp, BUFFER_SIZE);
-	while ((readcount))
+	while (readcount)
 	{
 		if (readcount == -1)
 			return (-1);
@@ -69,5 +79,19 @@ int	get_next_line(int fd, char **line)
 	buff[fd] = ft_line(buff[fd], line, readcount);
 	if (readcount <= 0 && !buff[fd])
 		return (readcount);
+	return (-2);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*buff[MAX_FD];
+	char		temp[BUFFER_SIZE + 1];
+	int			check;
+
+	if (!line || fd < 0 || fd > MAX_FD || BUFFER_SIZE < 0)
+		return (-1);
+	check = ft_check(fd, line, buff, temp);
+	if (check != -2)
+		return (check);
 	return (1);
 }
